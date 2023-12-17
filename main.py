8529665ead7bf634b77f184dfb4d48466cd9c87b
guala <gualandi.lorenzo@gmail.com>
2023-12-17 13:10:49 +0100

    cleanup

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+'''
+Esercizio python algoritmi grafo
+
+Esercizio per esame algoritmi sessione invernale 23/24
+input: matrice di adiacenza del grafo, con numeri delimitati da ,
+output: se il grafo e' orientato  o meno
+        percorso applicando bfs
+'''
+
+
 import csv
 from collections import deque
 import numpy as np
